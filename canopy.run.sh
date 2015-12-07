3ccdb53159764f938a839a8ea6b62aa8ef6b1e02
Ron Lee <leerw@ornl.gov>
2015-12-07 10:10:04 -0500

    Update of canopy.run.sh after testing

--- canopy.run.sh
+++ canopy.run.sh
@@ -5,5 +5,8 @@ CanopyBinDir=$HOME/Library/Enthought/Canopy_64bit/User/bin
 WxPythonLibDir=$HOME/Library/Enthought/Canopy_64bit/User/lib/wxPython/lib
 
 export DYLD_LIBRARY_PATH=${WxPythonLibDir}:${DYLD_LIBRARY_PATH}
+export ETS_TOOLKIT=wx
+#export PYTHONHOME=${WxPythonLibDir}/python2.7/site-packages/wx-3.0-osx_cocoa:${PYTHONHOME}
+
 
 exec ${CanopyBinDir}/python "$@"
