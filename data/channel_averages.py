Andrew fixed channel averages.

data/channel_averages.py
@@ -44,6 +44,7 @@ be called before use.
 			cannot be calculated for this dataset
 """
     avg = None
+    factor_weights = None
     if dset is not None:
       errors_save = np.seterr( divide = 'ignore', invalid = 'ignore' )
       try:
@@ -65,14 +66,24 @@ be called before use.
 
 	if factor_weights is None:
           factor_weights = np.ones(
-	      ( self.core.npiny + 1, self.core.npinx + 1,
+	      ( self.core.nchany, self.core.nchanx,
 	        self.core.nax, self.core.nass ),
-	      dtype = int
+	      dtype = np.float64
 	      )
-	else:
-	  pass
-	  #same stuff as pins _calc_weights() sans ref_pin_powers correction
-	#end if-else factor_weights
+	  for x in xrange( 0, self.core.nchanx ):
+	    factor_weights[ 0 , x, :, : ] *= 0.5
+	    factor_weights[ self.core.nchany - 1, x, :, : ] *= 0.5
+	  for y in xrange( 0, self.core.nchany ):
+	    factor_weights[ y , 0, :, : ] *= 0.5
+	    factor_weights[ y , self.core.nchanx - 1, :, : ] *= 0.5
+
+	  for k in xrange( self.core.nax ):
+	    factor_weights[ :, :, k, : ] *= \
+	        (self.core.axialMesh[ k + 1 ] - self.core.axialMesh[ k ])
+	#end if factor_weights is None
+
+#xxxxx Might need to *NOT* half symmetry line weights when COBRA-TF fixes
+	self.fix_weights( self.core, factor_weights )
 
         avg_weights = np.sum( factor_weights, axis = avg_axis )
 	#dset.value is deprecated
@@ -85,7 +96,7 @@ be called before use.
 	np.seterr( **errors_save )
     #end if
 
-    return  avg
+    return  avg, factor_weights
   #end calc_average
 
 
@@ -128,6 +139,45 @@ be called before use.
   def calc_channel_radial_avg( self, dset ):
     return  self.calc_average( dset, 2 )
   #end calc_channel_radial_avg
+
+
+  #----------------------------------------------------------------------
+  #	METHOD:		fix_weights()					-
+  #----------------------------------------------------------------------
+  def fix_weights( self, core, wts ):
+    """Zero outside the line of symmetry.
+"""
+    if core.coreSym == 4:
+      mass = core.nassx / 2
+      mpin = core.nchanx / 2
+      odd = core.nchanx % 2 == 1
+      pxlo = np.zeros( [ core.nass ], dtype = int )
+      pylo = np.zeros( [ core.nass ], dtype = int )
+#			-- Assemblies on the line of symmetry start at the
+#			-- middle channel
+      for i in xrange( mass, core.nassx ):
+        pxlo[ core.coreMap[ i, mass ] - 1 ] = mpin
+        pylo[ core.coreMap[ mass, i ] - 1 ] = mpin
+
+      for i in xrange( mass, core.nassx ):
+	assy_ndx = core.coreMap[ mass, i ] - 1
+	if assy_ndx >= 0:
+	  wts[ 0 : pylo[ assy_ndx ], 0 : core.nchanx, :, assy_ndx ] = 0.0
+	  if odd:
+	    wts[ pylo[ assy_ndx ], 0 : core.nchanx, :, assy_ndx ] *= 0.5
+      #end for i
+
+      for j in xrange( mass, core.nassy ):
+	assy_ndx = core.coreMap[ j, mass ] - 1
+	if assy_ndx >= 0:
+          wts[ 0 : core.nchany, 0 : pxlo[ assy_ndx ], :, assy_ndx ] = 0.0
+	  if odd:
+            wts[ 0 : core.nchany, pxlo[ assy_ndx ], :, assy_ndx ] *= 0.5
+      #end for j
+    #end if core.coreSym
+   
+    return  wts
+  #end fix_weights
 
 
   #----------------------------------------------------------------------

data/datamodel.py
@@ -481,6 +481,8 @@ Properties:
   nassx			number of core assembly columns
   nassy			number of core assembly rows
   nax			number of axial levels
+  nchanx
+  nchany
   ndet			number of detectors
   ndetax		number of detector axial levels
   nfdetax		number of fixed_detector axial levels
@@ -584,6 +586,7 @@ Properties:
     self.nassx = 0
     self.nassy = 0
     self.nax = 0
+    self.nchanx = self.nchany = 0
     self.ndet = 0
     self.ndetax = 0
     self.nfdetax = 0
@@ -611,6 +614,7 @@ Properties:
 #		--
     for name in (
 	'apitch', 'coreSym', 'group', 'nass', 'nassx', 'nassy', 'nax',
+	'nchanx', 'nchany',
         'ndet', 'ndetax', 'nfdetax', 'npin', 'npinx', 'npiny',
         'pinVolumesSum', 'ratedFlow', 'ratedPower'
         ):
@@ -1056,7 +1060,6 @@ to be column and row labels, respectively, and the order is forced such that
     if item is not None:
       self.npiny = item[ 0 ] if len( item.shape ) > 0 else item[ () ]
 
-
 #		-- Other datasets: pin_volumes
 #		--
     self.pinVolumesSum = 0
@@ -1142,6 +1145,9 @@ to be column and row labels, respectively, and the order is forced such that
         #self.npin = num_pins_ds.value[ 0 ]
     #end if
 
+    self.nchanx = self.npinx + 1
+    self.nchany = self.npiny + 1
+
 #		-- Assert NAX match b/w axial_mesh and pin_volumes
 #		--  Rely on call to Check() after DataModel.Read()
 #    if self.nax > 0 and self.pinVolumes is not None and \
@@ -1203,6 +1209,7 @@ Properties:
   dataSetDefs		dict of dataset definitions
   dataSetDefsByName	reverse lookup of dataset definitions by ds_name
   dataSetDefsLock	threading.RLock for dataSetDefs and dataSetDefsByName
+  dataSetFactors	dynamically created factors
   dataSetNames		dict of dataset names by category
 			  ( 'channel', 'derived', 'detector',
 			    'fixed_detector', 'pin', 'scalar' )
@@ -1375,7 +1382,8 @@ passed, Read() must be called.
     self.core = None
     self.dataSetDefs = {}
     self.dataSetDefsByName = {}
-    self.dataSetNames = []
+    self.dataSetFactors = {}
+    self.dataSetNames = {}
     self.dataSetNamesVersion = 0
     self.derivableTypesByLabel = {}
     self.derivedFile = None
@@ -1539,6 +1547,7 @@ Parameters:
 
 	try:
 	  avg_method = getattr( averager, avg_method_name )
+#x	  factors_ds_name = None
 
 #xxxxx will need to make this a separate thread with per-state progress feedback
           for state_ndx in xrange( len( self.states ) ):
@@ -1550,12 +1559,27 @@ Parameters:
 	      dset = derived_st.GetDataSet( ds_name )
 
 	    if dset is not None:
-	      avg_data = avg_method( dset )  # was data.value
+	      #avg_data = avg_method( dset )  # was data.value
+	      factors = None
+	      result = avg_method( dset )
+	      if hasattr( result, '__iter__' ):
+	        avg_data = result[ 0 ]
+		factors = result[ 1 ]
+	      else:
+	        avg_data = result
 	      derived_st.CreateDataSet( derived_name, avg_data )
+
+#x	      if factors is not None:
+#x	        factors_ds_name = ds_name + '_factors'
+#x	        st.CreateDataSet( factors_ds_name, factors )
 	    #end if data
 	  #end for each state
 
 	  self.AddDataSetName( der_names[ 0 ], derived_name )
+#x	  if factors_ds_name:
+#x            ddef = self.GetDataSetDefByDsName( ds_name )
+#x	    self.AddDataSetName( ddef[ 'type' ], factors_ds_name )
+#x	    self.dataSetFactors[ ds_name ] = factors
 
 	except Exception, ex:
 	  msg = 'Error calculating derived "%s" dataset for "%s"' % \
@@ -2952,43 +2976,41 @@ derived datasets.  Lazily created and cached.
 @param  dset		dataset to match
 @return			factors np.ndarray or None
 """
-    result = None
+    result = self.dataSetFactors.get( ds_name )
 
-#		-- Find dataset type
-#		--
-#    if ds_name and self.GetStatesCount() > 0:
-#      dset = self.GetStateDataSet( 0, ds_name )
-    ddef = self.GetDataSetDefByDsName( ds_name ) if ds_name else none
-    if ddef:
-      #result = self.pinFactors
-      ds_type = ddef[ 'type' ]
-      result = \
+    if result is None:
+      ddef = self.GetDataSetDefByDsName( ds_name ) if ds_name else none
+      if ddef:
+        #result = self.pinFactors
+        ds_type = ddef[ 'type' ]
+        result = \
           self.nodeFactors if self.IsNodalType( ds_type ) else self.pinFactors
-
-      if ddef[ 'type' ] == 'channel':
-        result = self.channelFactors
-
-      elif ddef[ 'type' ] == ':chan_radial':
-        result = np.ndarray( ddef[ 'copy_shape' ], dtype = np.float64 )
-        result.fill( 0.0 )
-	factors_sum = np.sum( self.channelFactors, axis = 2 )
-	exec_str = 'result' + ddef[ 'copy_expr' ] + ' = factors_sum'
-	exec(
-	    exec_str, {},
-	    { 'factors_sum': factors_sum, 'result': result }
-	    )
-
-      elif 'factors' in ddef and 'copy_shape' in ddef and \
-          'pin' in self.averagers:
-        result = np.ndarray( ddef[ 'copy_shape' ], dtype = np.float64 )
-        result.fill( 0.0 )
-	exec_str = \
-	    'result' + ddef[ 'copy_expr' ] + ' = averager.' + ddef[ 'factors' ]
-	exec(
-	    exec_str, {},
-	    { 'averager': self.averagers[ 'pin' ], 'result': result }
-	    )
-    #end if ddef
+  
+        if ddef[ 'type' ] == 'channel':
+          result = self.channelFactors
+  
+        elif ddef[ 'type' ] == ':chan_radial':
+          result = np.ndarray( ddef[ 'copy_shape' ], dtype = np.float64 )
+          result.fill( 0.0 )
+  	  factors_sum = np.sum( self.channelFactors, axis = 2 )
+  	  exec_str = 'result' + ddef[ 'copy_expr' ] + ' = factors_sum'
+  	  exec(
+  	      exec_str, {},
+  	      { 'factors_sum': factors_sum, 'result': result }
+  	      )
+  
+        elif 'factors' in ddef and 'copy_shape' in ddef and \
+            'pin' in self.averagers:
+          result = np.ndarray( ddef[ 'copy_shape' ], dtype = np.float64 )
+          result.fill( 0.0 )
+  	  exec_str = \
+  	    'result' + ddef[ 'copy_expr' ] + ' = averager.' + ddef[ 'factors' ]
+  	  exec(
+  	      exec_str, {},
+  	      { 'averager': self.averagers[ 'pin' ], 'result': result }
+  	      )
+      #end if ddef
+    #end if result is None
 
     return  result
   #end GetFactors