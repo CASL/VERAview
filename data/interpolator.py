fda1ad3d16298e75cd68dee96794d10d9195cd28
Ron Lee <leerw@ornl.gov>
2017-02-09 07:28:51 -0500

    WIP on interpolator take 3

--- data/interpolator.py
+++ data/interpolator.py
@@ -1,6 +1,8 @@
 #------------------------------------------------------------------------
 #	NAME:		interpolator.py					-
 #	HISTORY:							-
+#		2017-02-09	leerw@ornl.gov				-
+#	  New, simple approach.
 #		2017-02-06	leerw@ornl.gov				-
 #	  New approach from Andrew and Ben.
 #		2016-11-04	leerw@ornl.gov				-
@@ -9,7 +11,7 @@
 import h5py, os, sys, timeit
 import numpy as np
 from scipy.interpolate import interp1d
-from scipy import array, integrate
+#from scipy import array, integrate
 import pdb
 
 
@@ -17,7 +19,7 @@ import pdb
 #	CLASS:		Interpolator					-
 #------------------------------------------------------------------------
 class Interpolator( object ):
-  """
+  """Instances interpolate a dataset in a single state point.
 """
 
 
@@ -32,23 +34,20 @@ class Interpolator( object ):
   #----------------------------------------------------------------------
   #	METHOD:		__init__()					-
   #----------------------------------------------------------------------
-  def __init__( self, src_mesh_centers, dst_mesh, listener = None ):
-    """Sets up to interpolate within a single state point.
-It is assumed that none of the parameters are None.  Otherwise you will get an
-exception.
-
+  def __init__( self, src_mesh_centers, dst_mesh_centers, listener = None ):
+    """Sets up for interpolation.
 @param  src_mesh_centers  axial mesh centers (np.ndarray)
-@param  dst_mesh	result mesh, not centers (np.ndarray)
+@param  dst_mesh_centers  result mesh centers (np.ndarray)
 @param  listener	optional callable to invoke with progress, prototype
 			callback( message, progress_value ), where
 			progress_value is [0,100]
 """
 #		-- Assertions
 #		--
-    assert src_mesh_centers is not None and len( src_mesh_centers ) > 0, \
-        'src_mesh_centers cannot be empty'
-    assert dst_mesh is not None and len( dst_mesh ) >= 2, \
-        'dst_mesh must have length ge 2'
+    assert \
+        src_mesh_centers is not None and len( src_mesh_centers ) > 0 and \
+        dst_mesh_centers is not None and len( dst_mesh_centers ) > 0, \
+        'mesh centers cannot be empty'
 
 #		-- Plow on
 #		--
@@ -57,7 +56,7 @@ exception.
       self.listeners.append( listener )
 
     self.srcMeshCenters = src_mesh_centers
-    self.dstMesh = dst_mesh
+    self.dstMeshCenters = dst_mesh_centers
   #end __init__
 
 
@@ -81,42 +80,27 @@ parameters ( cur_step, total_steps ).
   #----------------------------------------------------------------------
   #	METHOD:		create_interpolator()				-
   #----------------------------------------------------------------------
-  def create_interpolator( self, xs, ys ):
-    f_s = interp1d( xs, ys, 'cubic' )
+  def create_interpolator( self, src_data, src_mesh_centers ):
+    """
+@return		interpolator function
+"""
+    f = interp1d(
+        src_mesh_centers, src_data,
+	assume_sorted = True, axis = 2, fill_value = 'extrapolate'
+	)
 
-    def get_y( x ):
-      if x < xs[ 0 ]:
-	if len( xs ) < 2 or len( ys ) < 2:
-	  y = ys[ 0 ]
-	else:
-	  y = ys[ 0 ] + \
-	      (x - xs[ 0 ]) * (ys[ 1 ] - ys[ 0 ]) / (xs[ 1 ] - xs[ 0 ])
-      elif x > xs[ -1 ]:
-        if len( xs ) < 2 or len( ys ) < 2:
-	  y = ys[ 0 ]
-        else:
-	  y = ys[ -1 ] + \
-	      (x - xs[ -1 ]) * (ys[ -1 ] - ys[ -2 ]) / (xs[ -1 ] - xs[ -2 ])
-      else:
-        #y = f_s( x )[ () ]
-        y = f_s( x )
-      return  y
-    #end extrap_pt
-
-    def interp_ys( xs_in ):
-      return  array( [ get_y( i ) for i in xs_in ] )
-
-    return  interp_ys
+    return  f
   #end create_interpolator
 
 
   #----------------------------------------------------------------------
   #	METHOD:		interpolate()					-
   #----------------------------------------------------------------------
-  def interpolate( self, src_data ):
+  def interpolate( self, src_data, f = None ):
     """
 @param  src_data	source dataset (h5py.Dataset or np.ndarray instance)
-@return			interpolated np.ndarray with len( self.dstMesh ) -1
+@param  f		interpolator function, will be created if None
+@return			interpolated np.ndarray with len( self.dstMeshCenters )
 			axial levels
 """
 #		-- Assertions
@@ -135,69 +119,31 @@ parameters ( cur_step, total_steps ).
 
     assert \
         src_data.shape[ 2 ] == len( self.srcMeshCenters ), \
-        'src_data has incompatible shapes'
-
+        'src_data has incompatible shape'
 
     start_time = timeit.default_timer()
 
     dst_shape = list( src_data.shape )
-    dst_shape[ 2 ] = len( self.dstMesh ) - 1
+    dst_shape[ 2 ] = len( self.dstMeshCenters )
 
     #dst_data = np.ndarray( dst_shape, dtype = np.float64 )
     dst_data = np.zeros( dst_shape, dtype = np.float64 )
 
-    step_count = reduce( (lambda x, y : x * y), dst_shape )
-    step_count = dst_shape[ 3 ]
-    step = 1
+    #step_count = reduce( (lambda x, y : x * y), dst_shape )
+    #step_count = dst_shape[ 3 ]
+    #step = 1
 
-    for assy_ndx in xrange( dst_shape[ 3 ] ):
-      self.notify_listeners( step, step_count )
-      for pin_row in xrange( dst_shape[ 0 ] ):
-        for pin_col in xrange( dst_shape[ 1 ] ):
-          #progress = int( 100 * assy_ndx / nass )
-          #self.notify_listeners( step, step_count )
-	  self.interpolate_slice(
-	      dst_data, self.dstMesh,
-	      src_data, self.srcMeshCenters,
-	      assy_ndx, pin_col, pin_row
-	      )
-	  #step += 1
-        #end for x
-      #end for y
-      step += 1
-    #end for assy_ndx
+    if f is None:
+      f = self.create_interpolator( src_data, self.srcMeshCenters )
+
+    for k in xrange( dst_shape[ 2 ] ):
+      dst_data[ :, :, k, : ] = f( self.dstMeshCenters[ k ] )
 
     elapsed_time = timeit.default_timer() - start_time
     print >> sys.stderr, '[interpolator] time=%.3fs' % elapsed_time
 
     return  dst_data
   #end interpolate
-
-
-  #----------------------------------------------------------------------
-  #	METHOD:		interpolate_slice()				-
-  #----------------------------------------------------------------------
-  def interpolate_slice( self,
-      dst_data, dst_mesh, src_data, src_mesh_centers,
-      assy_ndx, pin_col, pin_row
-      ):
-    """Interpolates a single slice.
-"""
-    f_e = self.create_interpolator(
-        src_mesh_centers,
-	src_data[ pin_row, pin_col, :, assy_ndx ]
-	)
-
-    for k in xrange( len( dst_mesh ) - 1 ):
-      a = dst_mesh[ k ]
-      b = dst_mesh[ k + 1 ]
-      result = integrate.quadrature( f_e, a, b )
-      value = result[ 0 ]  if hasattr( result, '__iter__' ) else  result
-      dst_data[ pin_row, pin_col, k, assy_ndx ] = value
-    #end for k
-
-    return  dst_data
-  #end interpolate_slice
 
 
   #----------------------------------------------------------------------
